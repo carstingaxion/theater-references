e0ac76288113ec1c86b8e8552d43f2aaba31fdd1
Carsten Bach <mail@carsten-bach.de>
2026-04-03 11:32:23 +0200

    ROLLBACK to wordpress/scripts 30.15.0, as the newer 31.8.0 produces a lot of "missing dependency" errors

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '677588f19a21ddf5171c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c71d639ea9090180bda0');
